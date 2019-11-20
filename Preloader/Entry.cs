8f00ba12796edb591ce2c2fe9c0db16311ecffa4
Joshua Hughes <info.creepingstudios@gmail.com>
2019-11-20 19:01:02 +1000

    Added a preloader to build filetree and install dependencies.

---
+++ Preloader/Entry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Preloader
+{
+    class Entry
+    {
+        public static void Main()
+        {
+            if (!Directory.Exists(Globals.CurrentDirectory + "bin"))
+            {
+                Directory.CreateDirectory(Globals.CurrentDirectory + "bin");
+            }
+            Globals.FingerPrint = Auth.GetMachineGuid();
+            Functions.DownloadAsset("Newtonsoft.Json.dll", Globals.CurrentDirectory + "bin\\Newtonsoft.Json.dll");
+            Functions.DownloadAsset("B4llista.exe", Globals.CurrentDirectory + "bin\\Ballista.exe");
+            Process.Start(Globals.CurrentDirectory + "bin\\Ballista.exe");
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Preloader/Globals.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preloader
+{
+    public class Globals
+    {
+        public static string AssetEndpoint = "http://45.77.236.84:81/asset/";
+        public static string FingerPrint = "";
+        public static string CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    }
+
+    public class Functions
+    {
+        public static void DownloadAsset(string URI, string Dest)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("authorization", "unsecure");
+                wc.Headers.Add("user-agent", "Ballista-" + Globals.FingerPrint);
+                wc.DownloadFile(Globals.AssetEndpoint + URI, Dest);
+            }
+            return;
+        }
+    }
+}
