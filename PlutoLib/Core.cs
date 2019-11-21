be4649cb8f26f83b9322a766225e66ca5cd7b8a3
Joshua Hughes <info.creepingstudios@gmail.com>
2019-11-21 10:10:59 +1000

    Added main lib.

---
+++ PlutoLib/Core.cs
@@ -0,0 +1,7 @@
+﻿namespace PlutoLib
+{
+    public class Core
+    {
+        
+    }
+}
---
+++ PlutoLib/Globals.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace PlutoLib
+{
+    public class Globals
+    {
+        public static string XSRFToken;
+        public static string Cookie;
+        public static JObject Items;
+
+        public static WebClient WebClient;
+    }
+
+    public class Functions
+    {
+        public static void RefreshWebClient()
+        {
+            if(Globals.WebClient != null) Globals.WebClient.Dispose();
+            Globals.WebClient = new WebClient();
+        }
+    }
+}
