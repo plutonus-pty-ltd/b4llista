67a31f926faece5c483d73aa1528245d30e29183
Joshua Hughes <info.creepingstudios@gmail.com>
2019-11-24 20:27:21 +1000

    Added BallistaLib and basic structures.

---
+++ BallistaLib/Auth.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+
+namespace BallistaLib
+{
+    public class Auth
+    {
+        public static void RegisterAuth()
+        {
+            JObject AuthObj = JObject.Parse(Functions.MakeApiRequest("auth/" + Globals.FingerPrint));
+
+            if ((int)AuthObj["Error"] != -1)
+            {
+                Error Err = Globals.Errors.Find(delegate (Error i) { return i.ErrorCode == (int)AuthObj["Error"]; });
+                Environment.Exit(1);
+            }
+
+            Globals.APIKey = (string)AuthObj["Token"];
+            return;
+        }
+    }
+
+    public class FingerPrint
+    {
+        public static string GetMachineGuid()
+        {
+            string location = @"SOFTWARE\Microsoft\Cryptography";
+            string name = "MachineGuid";
+
+            using (RegistryKey localMachineX64View =
+                RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                using (RegistryKey rk = localMachineX64View.OpenSubKey(location))
+                {
+                    if (rk == null)
+                        throw new KeyNotFoundException(
+                            string.Format("Key Not Found: {0}", location));
+
+                    object machineGuid = rk.GetValue(name);
+                    if (machineGuid == null)
+                        throw new IndexOutOfRangeException(
+                            string.Format("Index Not Found: {0}", name));
+
+                    return machineGuid.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ BallistaLib/Globals.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net;
+
+namespace BallistaLib
+{
+    public class Globals
+    {
+        public static string BaseEndpoint = "http://45.77.236.84:81/";
+        public static string FingerPrint = "";
+        public static string APIKey = "";
+
+        public static string XSRFToken;
+        public static string Cookie;
+        public static JObject Items;
+
+        public static WebClient WebClient;
+
+        public static List<Error> Errors = new List<Error>()
+        {
+            new Error()
+            {
+                ErrorCode = -1,
+                ErrorMessage = "OK"
+            },
+            new Error()
+            {
+                ErrorCode = 4000,
+                ErrorMessage = "Access to this resource is denied."
+            },
+            new Error()
+            {
+                ErrorCode = 4001,
+                ErrorMessage = "Invalid authorization token."
+            },
+            new Error()
+            {
+                ErrorCode = 4002,
+                ErrorMessage = "Ballista is currently down for maintainence."
+            },
+            new Error()
+            {
+                ErrorCode = 4003,
+                ErrorMessage = "Suspicious request."
+            },
+            new Error()
+            {
+                ErrorCode = 4004,
+                ErrorMessage = "Authorization failed."
+            },
+            new Error()
+            {
+                ErrorCode = 4005,
+                ErrorMessage = "Unknown account holder."
+            }
+        };
+    }
+
+    public class Error
+    {
+        public int ErrorCode { get; set; } = -1;
+        public string ErrorMessage { get; set; } = "Unknown error.";
+    }
+
+    public class Functions
+    {
+        public static void RefreshWebClient()
+        {
+            if (Globals.WebClient != null) Globals.WebClient.Dispose();
+            Globals.WebClient = new WebClient();
+        }
+
+        public static string MakeApiRequest(string URI)
+        {
+            return Globals.WebClient.DownloadString(Globals.BaseEndpoint + URI);
+        }
+    }
+}
